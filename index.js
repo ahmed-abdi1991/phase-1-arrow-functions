049a1572c8e93db89f85ccfe0c93f8e9776f0166
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-02 07:13:37 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const divide = function () {
+  return 2000 / 100;
+};
+const square = (x) => {
+  return x * x;
+}
+const add = (a, b) => {
+  return a + b;
+};
+console.log("divide(2000 / 100):", divide());
+console.log("square(5 * 5):", square(5));
+console.log("add(10 + 5):", add(10, 5));
